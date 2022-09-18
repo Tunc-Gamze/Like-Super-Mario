1b8faaee1905f868ba371fb16a6eec36a93e777f
Gamze Tunc <ggamzettunc@gmail.com>
2022-09-18 14:08:59 +0300

    Idle,Run and Flip Face completed.

---
+++ playerController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+ 
+    Rigidbody2D playerRB;
+    Animator playerAnimator;
+    public float moveSpeed=1f;
+    bool facingRight=true;
+
+
+    void Awake()
+    {
+
+    }
+    void Start()
+    {
+        playerRB=GetComponent<Rigidbody2D>();
+        playerAnimator=GetComponent<Animator>();
+    }
+
+    
+    void Update()
+    {
+       HorizontalMove();
+       if(playerRB.velocity.x<0 && facingRight )
+       {
+        FlipFace();
+       }
+       else if(playerRB.velocity.x>0 && !facingRight)
+       {
+        FlipFace();
+       }
+    }
+    void FixedUpdate()
+    {
+
+    }
+    void HorizontalMove()
+    {
+        playerRB.velocity=new Vector2(Input.GetAxis("Horizontal")*moveSpeed,playerRB.velocity.y);
+        playerAnimator.SetFloat("playerSpeed",Mathf.Abs(playerRB.velocity.x));
+
+    }
+    
+
+    void FlipFace()
+    {
+        facingRight=!facingRight;
+        Vector3 temLocalScale=transform.localScale;
+        temLocalScale.x*=-1;
+        transform.localScale=temLocalScale;
+    }
+}
